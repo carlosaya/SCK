0746115b50cb305a223a9740deca1504547fc33a
Carlos Aya <carlos.aya@tabcorp.com.au>
2023-04-20 08:48:59 +1000

    update config

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master"
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINA_PUBLIC_CLIENT_ID, // Get this from tina.io
-  token: process.env.TINA_TOKEN, // Get this from tina.io
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID!, // Get this from tina.io
+  token: process.env.TINA_TOKEN!, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
